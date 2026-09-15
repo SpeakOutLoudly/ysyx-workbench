9483f5109bd4599b00490f5bcb2d91fbccf12d91
lin <speakoutloudly@163.com>
2026-09-15 11:09:15 +0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard 4b11dedf1e8a568ad7f74aa68557648b679a9264 README: refine for calling nvboard_update() every cycles

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,27 @@
-#include <stdio.h>
+#include "Vtop.h"
+#include "verilated.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+int main(int argc, char **argv) {
+    VerilatedContext context;
+    context.commandArgs(argc, argv);
+
+    Vtop top{&context};
+
+    top.reset = 1;
+
+    for (int cycle = 0; cycle < 20; cycle++) {
+        top.clk = 0;
+        top.eval();
+
+        top.clk = 1;
+        top.eval();
+
+        if (cycle == 2)
+            top.reset = 0;
+
+        printf("cycle=%d count=%d\n",
+               cycle, top.count);
+    }
+
+    return 0;
 }
